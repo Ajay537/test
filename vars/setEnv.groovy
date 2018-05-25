570be7b0eeca9fe587a981c34a861e01bebf672b
Ajay <mandavilli.ajay@gmail.com>
2018-05-25 17:36:43 +0530

    Update setEnv.groovy

--- vars/setEnv.groovy
+++ vars/setEnv.groovy
@@ -1,6 +1,6 @@
 def call(Map config){
     def name = config.name
-    withEnv(['config.name=config.path']{
-        sh "echo ${name}"    
-    }
+    def path = config.path
+    sh "echo ${name}"
+    sh "echo ${path}"
 }
