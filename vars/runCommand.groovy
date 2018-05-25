02867f6e4bdb65691f4c78b0da193775152b6ec3
Ajay M Gupta Mandavilli <ajaymgupta.mandavilli@factset.com>
2018-05-25 16:56:13 +0530

    added map

--- vars/runCommand.groovy
+++ vars/runCommand.groovy
@@ -1,7 +1,7 @@
-def call(String command, int timeout){
+def call(Map config){
     def sout = new StringBuilder(), serr = new StringBuilder()
-    def proc = "${command}".execute()
+    def proc = "${config.command}".execute()
     proc.consumeProcessOutput(sout, serr)
-    proc.waitForOrKill(timeout)
+    proc.waitForOrKill(config.timeout)
     println "out> $sout err> $serr"
 }
